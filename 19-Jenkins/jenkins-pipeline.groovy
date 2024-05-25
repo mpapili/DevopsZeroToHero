720d36969d81679b7240877fc038091c14d55f10
mpapili <michaelpapili@gmail.com>
2024-05-25 15:58:04 -0400

    Jenkins app

---
+++ 19-Jenkins/jenkins-pipeline.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+    stages {
+        stage("test-stage1") {
+            steps {
+                // say which worker we're using
+                echo "hello world from ${env.HOSTNAME}"
+            }
+        }
+        stage("run-tests") {
+            steps {
+                echo "running unit tests in docker container"
+                script {
+                    // grab our desired docker image
+                    def dockerImage = docker.image('test-py')
+                    // run the docker image
+                    dockerImage.inside('--rm') {
+                        // Execute commands inside the Docker container
+                        sh 'echo "Running inside Docker"'
+                        sh 'python3 /app/my_script.py'
+                    }
+                }
+            }
+        }
+        stage("check-format") {
+            steps {
+                script {
+                    def dockerImage = docker.image('test-py')
+                    dockerImage.inside('--rm') {
+                        // Execute commands inside the Docker container
+                        sh 'echo "Running inside Docker"'
+                        sh 'cd /app'
+                        sh 'flake8 .'
+                    }
+                }
+            }
+        }
+    }
+}
